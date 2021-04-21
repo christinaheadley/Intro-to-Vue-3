2e1add5d3aeb95fe65f16de020aed58aea6b01ce
christina <chrstnaheadley@gmail.com>
2021-04-21 19:23:44 -0400

    adds description to data

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description: "These blue wool socks will keep your feet warm in wintertime.",
+    };
+  },
+});
